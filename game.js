cd6aca0f7efd2ea8755935dc04218bf48dbd7352
Darin Gillis <dgillis@MacBook-Pro-2.local>
2018-08-12 22:19:49 -0500

    JSLint warnings round 1: fix all of the spacing and ordering issues

--- game.js
+++ game.js
@@ -1,23 +1,22 @@
-// https://vimeo.com/105955605
-(function() {
-    var Game = function(canvasId) {
+// code typed out here: https://vimeo.com/105955605
+// getting rid of "this" keywords to fix JSLint errors:
+// https://stackoverflow.com/questions/30314944/jslint-error-unexpected-this/30375300#30375300
+"use strict";
+(function () {
+    var Game = function (canvasId) {
         var canvas = document.getElementById(canvasId);
         var screen = canvas.getContext('2d');
-        var gameSize = { x: canvas.width, y: canvas.height };
-        //var framesPerSecond = 10;
-        this.bodies = createInvaders(this).concat(new Player(this, gameSize));
-
+        var gameSize = {x: canvas.width, y: canvas.height};
         var self = this;
+        self.bodies = createInvaders(self).concat(new Player(self, gameSize));
+
         // laser sound from http://www.findsounds.com/ISAPI/search.dll?keywords=laser
-        loadSound("shoot.wav", function(shootSound) {
+        loadSound("shoot.wav", function (shootSound) {
             self.shootSound = shootSound;
-            var tick = function() {
+            var tick = function () {
                 self.update(gameSize);
                 self.draw(screen, gameSize);
-                // uncomment these lines to slow down the animation
-                //setTimeout(function() {
-                    requestAnimationFrame(tick);
-                    //}, 1000 / framesPerSecond);
+                requestAnimationFrame(tick);
             };
 
             tick();
@@ -25,56 +24,61 @@
     };
 
     Game.prototype = {
-        update: function(gameSize) {
+        update: function (gameSize) {
             var bodies = this.bodies;
-            var notCollingWithAnything = function(b1) {
-                return bodies.filter(function(b2) { return colliding(b1, b2); }).length === 0;
+            var notCollingWithAnything = function (b1) {
+                return bodies.filter(function (b2) {
+                    return colliding(b1, b2);
+                }).length === 0;
             };
+            var i;
             this.bodies = this.bodies.filter(notCollingWithAnything);
-            for (var i=0; i < this.bodies.length; i++) {
+            //this.bodies.foreach(update);
+            for (i = 0; i < this.bodies.length; i += 1) {
                 this.bodies[i].update(gameSize);
             }
         },
-        draw: function(screen, gameSize) {
-            screen.clearRect(0,0,gameSize.x, gameSize.y);
-            for (var i=0; i < this.bodies.length; i++) {
+        draw: function (screen, gameSize) {
+            var i;
+            screen.clearRect(0, 0, gameSize.x, gameSize.y);
+            for (i = 0; i < this.bodies.length; i += 1) {
                 drawRect(screen, this.bodies[i]);
             }
         },
-        addBody: function(body) {
+        addBody: function (body) {
             this.bodies.push(body);
         },
-        invadersBelow: function(invader) {
-            return this.bodies.filter(function(b) {
-                return b instanceof Invader && 
-                    b.center.y > invader.center.y &&
-                    b.center.x - invader.center.x < invader.size.x;
+        invadersBelow: function (invader) {
+            return this.bodies.filter(function (b) {
+                return b instanceof Invader &&
+                        b.center.y > invader.center.y &&
+                        b.center.x - invader.center.x < invader.size.x;
             }).length > 0;
         }
     };
 
-    var Player = function(game, gameSize) {
+    var Player = function (game, gameSize) {
         this.game = game;
-        this.size = { x: 15, y: 15 };
-        this.center = { x: gameSize.x / 2, y: gameSize.y - this.size.x};
+        this.size = {x: 15, y: 15};
+        this.center = {x: gameSize.x / 2, y: gameSize.y - this.size.x};
         this.keyboarder = new Keyboarder();
     };
 
-    Player.prototype = { 
-        update: function(gameSize) {
+    Player.prototype = {
+        update: function (item, index, arr) {
             if (this.keyboarder.isDown(this.keyboarder.KEYS.LEFT)) {
                 this.center.x -= 2;
-                if (this.center.x < 0 + this.size.x/2) {
-                    this.center.x = this.size.x/2;
-                }
+                //if (this.center.x < 0 + this.size.x/2) {
+                //    this.center.x = this.size.x/2;
+                //}
             } else if (this.keyboarder.isDown(this.keyboarder.KEYS.RIGHT)) {
                 this.center.x += 2;
-                if (this.center.x > gameSize.x - this.size.x/2) {
-                    this.center.x = gameSize.x - this.size.x/2;
-                }
+                //if (this.center.x > gameSize.x - this.size.x/2) {
+                //    this.center.x = gameSize.x - this.size.x/2;
+                //}
             }
             if (this.keyboarder.isDown(this.keyboarder.KEYS.SPACE)) {
-                var bullet = new Bullet( { x: this.center.x, y: this.center.y - this.size.x /2}, {x: 0, y: -6});
+                var bullet = new Bullet({x: this.center.x, y: this.center.y - this.size.x / 2}, {x: 0, y: -6});
                 this.game.addBody(bullet);
                 this.game.shootSound.load();
                 this.game.shootSound.play();
@@ -82,63 +86,66 @@
         }
     };
 
-    var Invader = function(game, center) {
+    var Invader = function (game, center) {
         this.game = game;
-        this.size = { x: 15, y: 15 };
+        this.size = {x: 15, y: 15};
         this.center = center;
         this.patrolX = 0;
         this.speedX = 0.3;
     };
 
-    Invader.prototype = { 
-        update: function(gameSize) {
-                if (this.patrolX < 0 || this.patrolX > 40) {
-                    this.speedX = -this.speedX;
-                }
-                this.center.x += this.speedX;
-                this.patrolX += this.speedX;
-
-                if (Math.random() > 0.995 && !this.game.invadersBelow(this)) {
-                    var bullet = new Bullet( { x: this.center.x, y: this.center.y + this.size.x /2}, {x: Math.random() - 0.5, y: 2 });
-                    this.game.addBody(bullet);
-                }
+    Invader.prototype = {
+        update: function (item, index, arr) {
+            if (this.patrolX < 0 || this.patrolX > 40) {
+                this.speedX = -this.speedX;
             }
+            this.center.x += this.speedX;
+            this.patrolX += this.speedX;
+
+            if (Math.random() > 0.995 && !this.game.invadersBelow(this)) {
+                var bullet = new Bullet({x: this.center.x, y: this.center.y + this.size.x / 2}, {x: Math.random() - 0.5, y: 2});
+                this.game.addBody(bullet);
+            }
+        }
     };
 
-    var createInvaders = function(game){
+    var createInvaders = function (game) {
         var invaders = [];
-        for (var i = 0; i < 24; i++) {
-            var x = 30 + (i % 8) * 30;
-            var y = 30 + (i % 3) * 30;
-            invaders.push(new Invader(game, { x: x, y: y}));
+        var i;
+        var x;
+        var y;
+        for (i = 0; i < 24; i += 1) {
+            x = 30 + (i % 8) * 30;
+            y = 30 + (i % 3) * 30;
+            invaders.push(new Invader(game, {x: x, y: y}));
         }
         return invaders;
-    }
+    };
 
-    var Bullet = function(center, velocity) {
-        this.size = { x: 3, y: 3 };
+    var Bullet = function (center, velocity) {
+        this.size = {x: 3, y: 3};
         this.center = center;
         this.velocity = velocity;
     };
 
-    Bullet.prototype = { 
-        update: function() {
+    Bullet.prototype = {
+        update: function (item, index, arr) {
             this.center.x += this.velocity.x;
             this.center.y += this.velocity.y;
         }
     };
 
     var drawRect = function (screen, body) {
-        screen.fillRect(body.center.x - body.size.x /2,
-                        body.center.y - body.size.y /2,
+        screen.fillRect(body.center.x - body.size.x / 2,
+                        body.center.y - body.size.y / 2,
                         body.size.x, body.size.y);
     };
 
-    var Keyboarder = function() {
+    var Keyboarder = function () {
         var keyState = {};
 
         //window.onkeyup didn't work so well for me, switched to document
-        document.onkeydown = function(e) {
+        document.onkeydown = function (e) {
             //console.log(e.type + ' key=' + e.key + ' keycode=' + e.keyCode);
             if (e.keyCode !== undefined) {
                 keyState[e.keyCode] = true;
@@ -146,38 +153,38 @@
         };
 
         //window.onkeyup didn't work so well for me, switched to document
-        document.onkeyup = function(e) {
+        document.onkeyup = function (e) {
             //console.log(e.type + ' key=' + e.key + ' keycode=' + e.keyCode);
             if (e.keyCode !== undefined) {
                 keyState[e.keyCode] = false;
             }
         };
 
-        this.isDown = function(keyCode) {
+        this.isDown = function (keyCode) {
             return keyState[keyCode] === true;
         };
 
-        this.KEYS = { LEFT: 37, RIGHT: 39, SPACE: 32 };
-    }
+        this.KEYS = {LEFT: 37, RIGHT: 39, SPACE: 32};
+    };
 
-    var colliding = function(b1, b2) {
-        return !(b1 === b2 || 
+    var colliding = function (b1, b2) {
+        return !(b1 === b2 ||
                 b1.center.x + b1.size.x / 2 < b2.center.x - b2.size.x / 2 ||
                 b1.center.y + b1.size.y / 2 < b2.center.y - b2.size.y / 2 ||
                 b1.center.x - b1.size.x / 2 > b2.center.x + b2.size.x / 2 ||
                 b1.center.y - b1.size.y / 2 > b2.center.y + b2.size.y / 2);
     };
 
-    var loadSound = function(url, callback) {
-        var loaded = function() {
+    var loadSound = function (url, callback) {
+        var loaded = function () {
             callback(sound);
             sound.removeEventListener('canplaythrough', loaded);
         };
-        var sound = new Audio (url);
+        var sound = new Audio(url);
         sound.addEventListener('canplaythrough', loaded);
         sound.load();
     };
-    window.onload = function() {
+    window.onload = function () {
         new Game('screen');
     };
 })();
