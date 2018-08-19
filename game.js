2e169a37cbe5610a91d319359f11d4ea3c136d95
Darin Gillis <dgillis@MacBook-Pro-2.local>
2018-08-18 21:32:23 -0500

    Fixed the linting errors for out of scope without closures

--- game.js
+++ game.js
@@ -11,44 +11,77 @@
 */
 "use strict";
 (function () {
-    var Game = function (canvasId) {
-        var canvas = document.getElementById(canvasId);
-        this.screen = canvas.getContext('2d');
-        this.gameSize = {x: canvas.width, y: canvas.height};
-        this.bodies = createInvaders(this).concat(new Player(this, this.gameSize));
+    var Bullet = function (center, velocity) {
+        this.size = {x: 3, y: 3};
+        this.center = center;
+        this.velocity = velocity;
     };
 
-    Game.prototype = {
+    Bullet.prototype = {
         update: function () {
-            var bodies = this.bodies;
-            var i;
-            var notCollingWithAnything = function (b1) {
-                return bodies.filter(function (b2) {
-                    return colliding(b1, b2);
-                }).length === 0;
-            };
-            this.bodies = this.bodies.filter(notCollingWithAnything);
-            for (i = 0; i < this.bodies.length; i += 1) {
-                this.bodies[i].update(this.gameSize);
-            }
-        },
-        draw: function () {
-            var i;
-            this.screen.clearRect(0, 0, this.gameSize.x, this.gameSize.y);
-            for (i = 0; i < this.bodies.length; i += 1) {
-                drawRect(this.screen, this.bodies[i]);
-            }
-        },
-        addBody: function (body) {
-            this.bodies.push(body);
-        },
-        invadersBelow: function (invader) {
-            return this.bodies.filter(function (b) {
-                return b instanceof Invader &&
-                        b.center.y > invader.center.y &&
-                        b.center.x - invader.center.x < invader.size.x;
-            }).length > 0;
+            this.center.x += this.velocity.x;
+            this.center.y += this.velocity.y;
         }
+    };
+
+    var Invader = function (game, center) {
+        this.game = game;
+        this.size = {x: 15, y: 15};
+        this.center = center;
+        this.patrolX = 0;
+        this.speedX = 0.3;
+    };
+
+    Invader.prototype = {
+        update: function () {
+            if (this.patrolX < 0 || this.patrolX > 40) {
+                this.speedX = -this.speedX;
+            }
+            this.center.x += this.speedX;
+            this.patrolX += this.speedX;
+
+            if (Math.random() > 0.995 && !this.game.invadersBelow(this)) {
+                var bullet = new Bullet({x: this.center.x, y: this.center.y + this.size.x / 2}, {x: Math.random() - 0.5, y: 2});
+                this.game.addBody(bullet);
+            }
+        }
+    };
+
+    var createInvaders = function (game) {
+        var invaders = [];
+        var i, x, y;
+        for (i = 0; i < 24; i += 1) {
+            x = 30 + (i % 8) * 30;
+            y = 30 + (i % 3) * 30;
+            invaders.push(new Invader(game, {x: x, y: y}));
+        }
+        return invaders;
+    };
+
+    var Keyboarder = function () {
+        var keyState = {};
+
+        //window.onkeyup didn't work so well for me, switched to document
+        document.onkeydown = function (e) {
+            //console.log(e.type + ' key=' + e.key + ' keycode=' + e.keyCode);
+            if (e.keyCode !== undefined) {
+                keyState[e.keyCode] = true;
+            }
+        };
+
+        //window.onkeyup didn't work so well for me, switched to document
+        document.onkeyup = function (e) {
+            //console.log(e.type + ' key=' + e.key + ' keycode=' + e.keyCode);
+            if (e.keyCode !== undefined) {
+                keyState[e.keyCode] = false;
+            }
+        };
+
+        this.isDown = function (keyCode) {
+            return keyState[keyCode] === true;
+        };
+
+        this.KEYS = {LEFT: 37, RIGHT: 39, SPACE: 32};
     };
 
     var Player = function (game, gameSize) {
@@ -94,85 +127,6 @@
         }
     };
 
-    var Invader = function (game, center) {
-        this.game = game;
-        this.size = {x: 15, y: 15};
-        this.center = center;
-        this.patrolX = 0;
-        this.speedX = 0.3;
-    };
-
-    Invader.prototype = {
-        update: function () {
-            if (this.patrolX < 0 || this.patrolX > 40) {
-                this.speedX = -this.speedX;
-            }
-            this.center.x += this.speedX;
-            this.patrolX += this.speedX;
-
-            if (Math.random() > 0.995 && !this.game.invadersBelow(this)) {
-                var bullet = new Bullet({x: this.center.x, y: this.center.y + this.size.x / 2}, {x: Math.random() - 0.5, y: 2});
-                this.game.addBody(bullet);
-            }
-        }
-    };
-
-    var createInvaders = function (game) {
-        var invaders = [];
-        var i, x, y;
-        for (i = 0; i < 24; i += 1) {
-            x = 30 + (i % 8) * 30;
-            y = 30 + (i % 3) * 30;
-            invaders.push(new Invader(game, {x: x, y: y}));
-        }
-        return invaders;
-    };
-
-    var Bullet = function (center, velocity) {
-        this.size = {x: 3, y: 3};
-        this.center = center;
-        this.velocity = velocity;
-    };
-
-    Bullet.prototype = {
-        update: function () {
-            this.center.x += this.velocity.x;
-            this.center.y += this.velocity.y;
-        }
-    };
-
-    var drawRect = function (screen, body) {
-        screen.fillRect(body.center.x - body.size.x / 2,
-                body.center.y - body.size.y / 2,
-                body.size.x, body.size.y);
-    };
-
-    var Keyboarder = function () {
-        var keyState = {};
-
-        //window.onkeyup didn't work so well for me, switched to document
-        document.onkeydown = function (e) {
-            //console.log(e.type + ' key=' + e.key + ' keycode=' + e.keyCode);
-            if (e.keyCode !== undefined) {
-                keyState[e.keyCode] = true;
-            }
-        };
-
-        //window.onkeyup didn't work so well for me, switched to document
-        document.onkeyup = function (e) {
-            //console.log(e.type + ' key=' + e.key + ' keycode=' + e.keyCode);
-            if (e.keyCode !== undefined) {
-                keyState[e.keyCode] = false;
-            }
-        };
-
-        this.isDown = function (keyCode) {
-            return keyState[keyCode] === true;
-        };
-
-        this.KEYS = {LEFT: 37, RIGHT: 39, SPACE: 32};
-    };
-
     var colliding = function (b1, b2) {
         return !(b1 === b2 ||
                 b1.center.x + b1.size.x / 2 < b2.center.x - b2.size.x / 2 ||
@@ -181,12 +135,60 @@
                 b1.center.y - b1.size.y / 2 > b2.center.y + b2.size.y / 2);
     };
 
+    var drawRect = function (screen, body) {
+        screen.fillRect(body.center.x - body.size.x / 2,
+                body.center.y - body.size.y / 2,
+                body.size.x, body.size.y);
+    };
+
+    var Game = function (canvasId) {
+        var canvas = document.getElementById(canvasId);
+        this.screen = canvas.getContext('2d');
+        this.gameSize = {x: canvas.width, y: canvas.height};
+        this.bodies = createInvaders(this).concat(new Player(this, this.gameSize));
+    };
+
+    Game.prototype = {
+        update: function () {
+            var bodies = this.bodies;
+            var i;
+            var notCollingWithAnything = function (b1) {
+                return bodies.filter(function (b2) {
+                    return colliding(b1, b2);
+                }).length === 0;
+            };
+            this.bodies = this.bodies.filter(notCollingWithAnything);
+            for (i = 0; i < this.bodies.length; i += 1) {
+                this.bodies[i].update(this.gameSize);
+            }
+        },
+        draw: function () {
+            var i;
+            this.screen.clearRect(0, 0, this.gameSize.x, this.gameSize.y);
+            for (i = 0; i < this.bodies.length; i += 1) {
+                drawRect(this.screen, this.bodies[i]);
+            }
+        },
+        addBody: function (body) {
+            this.bodies.push(body);
+        },
+        invadersBelow: function (invader) {
+            return this.bodies.filter(function (b) {
+                return b instanceof Invader &&
+                        b.center.y > invader.center.y &&
+                        b.center.x - invader.center.x < invader.size.x;
+            }).length > 0;
+        }
+    };
+
     var loadSound = function (url, callback) {
-        var loaded = function () {
+        var sound = new Audio(url);
+        var loaded;
+        // http://blog.boyet.com/blog/javascriptlessons/jslint-and-recursive-functions-in-javascript/
+        loaded = function () {
             callback(sound);
             sound.removeEventListener('canplaythrough', loaded);
         };
-        var sound = new Audio(url);
         sound.addEventListener('canplaythrough', loaded);
         sound.load();
     };
